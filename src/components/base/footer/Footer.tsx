14d78262cfb1d6d05153e0f7cdd358be4d3e3c1d
Sebastian Ness <sebastian.ness@gmail.com>
2024-02-16 20:40:58 +0100

    Fixed wrong imports

--- src/components/base/footer/Footer.tsx
+++ src/components/base/footer/Footer.tsx
@@ -1,10 +1,9 @@
+import somed_email from '@/assets/some_icons/email_white.png';
+import somed_facebook from '@/assets/some_icons/facebook_white.png';
+import somed_instagram from '@/assets/some_icons/instagram_white.png';
+import somed_youtube from '@/assets/some_icons/youtube_white.png';
+import { PageContainer } from '@/layout/PageContainer';
 import React from 'react';
-import somed_email from '../../../assets/somed_icons/email_white.png';
-import somed_facebook from '../../../assets/somed_icons/facebook_white.png';
-import somed_instagram from '../../../assets/somed_icons/instagram_white.png';
-import somed_youtube from '../../../assets/somed_icons/youtube_white.png';
-import '../../../css/styles.css';
-import PageContainer from '../../layout/PageContainer';
 import SocialMediaLink from './SocialMediaLink';
 
 const Footer: React.FC = () => {
@@ -15,21 +14,21 @@ const Footer: React.FC = () => {
       <PageContainer>
         <hr />
       </PageContainer>
-      <div className="somed">
-        <SocialMediaLink href="mailto:post@offh.no" title="Send email" src={somed_email} />
-        <SocialMediaLink href="https://www.youtube.com/@OFFH2024" title="Link to our Youtube" src={somed_youtube} />
+      <div className='somed'>
+        <SocialMediaLink href='mailto:post@offh.no' title='Send email' src={somed_email} />
+        <SocialMediaLink href='https://www.youtube.com/@OFFH2024' title='Link to our Youtube' src={somed_youtube} />
         <SocialMediaLink
-          href="https://facebook.com/OneFloorFromHeaven"
-          title="Link to our Facebook"
+          href='https://facebook.com/OneFloorFromHeaven'
+          title='Link to our Facebook'
           src={somed_facebook}
         />
         <SocialMediaLink
-          href="https://instagram.com/OneFloorFromHeaven"
-          title="Link to our Instagram"
+          href='https://instagram.com/OneFloorFromHeaven'
+          title='Link to our Instagram'
           src={somed_instagram}
         />
       </div>
-      <div className="copyright">© One Floor From Heaven {currentYear}</div>
+      <div className='copyright'>© One Floor From Heaven {currentYear}</div>
     </footer>
   );
 };
